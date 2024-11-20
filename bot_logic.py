bb390e3b9b93931323393fafb120bfe9e9e860c0
onlyvvova <v.egorov361@kodland.team>
2024-11-20 23:56:49 +0300

    wdsdsd sdsdsd

--- bot_logic.py
+++ bot_logic.py
@@ -1,8 +1,3 @@
 import random
-
-def gen_pass(pass_length):
-    elements = "+-/*!&$#?=@<>12sdsdsdsd3456789"
-    password = ""
-    for i in range(pass_length):
-        password += random.choice(elements)
-    return password
+file = open("C:\\Users\\evova\\OneDrive\\Рабочий стол\\TestProject\\M1L3\\bot.py", "r", encoding='utf-8')
+print(file.name)
--- main.py
+++ main.py
@@ -1,27 +1,58 @@
 import telebot
-from bot_logic import gen_pass    
+from telebot import types   
 # Замени 'TOKEN' на токен твоего бота
 # Этот токен ты получаешь от BotFather, чтобы бот мог работать
 bot = telebot.TeleBot("7295431439:AAG5j42KN6-ajiOQaY_vERBofSnmdg_z5M0")
     
-@bot.message_handler(commands=['start'])
-def send_welcome(message):
-    bot.reply_to(message, "Привет! Я твой Telegram бот. Напиши что-нибудь!")
+@bot.message_handler(commands=['help', 'start'])
+def home(message):
+    bot.reply_to(message, """\
+Hi there, I am EchoBot.
+I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
+""")
+@bot.message_handler(func=lambda x: x.text == "1")
+def tut(message):
+    bot.send_message(message.chat.id, """Works def tut""") 
+           
+@bot.message_handler(commands=["key"])
+def key(message):                                          # Bottoni che quando clicchi fanno il testo
+    kb= types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn1 = types.KeyboardButton("Ciao")
+    btn2 = types.KeyboardButton("Arrivedrci")
+    kb.add(btn1,btn2)
+    bot.send_message(message.chat.id, "Ciao", reply_markup=kb)
+  
+@bot.message_handler(commands=["button"])  
+def key(message):
+    kb = types.InlineKeyboardMarkup(row_width=2)                            # Bottoni che quando clicchi ti portano ad un sito con url
+    btn1 = types.InlineKeyboardButton("gpt", url="https://chatgpt.com/")
+    btn2 = types.InlineKeyboardButton("erewhon", url="https://erewhon.com/" )
+    kb.add(btn1,btn2)
+    bot.send_message(message.chat.id, "Here are some buttons", reply_markup=kb)
     
-@bot.message_handler(commands=['hello'])
-def send_hello(message):
-    bot.reply_to(message, "Привет! Как дела?")
-    
-@bot.message_handler(commands=['bye'])
-def send_bye(message):
-    bot.reply_to(message, "Пока! Удачи!")
 
-@bot.message_handler(commands=['pass'])
-def send_pass(message):
-    password = gen_pass(10)
-    bot.reply_to(message, f"Пароль: {password}")
+@bot.message_handler(commands=["car"])
+def car(message):
+    telebot.util.extract_arguments(message.text) 
 
     
+@bot.message_handler(commands=["test"])
+def switch(message):
+    markup = types.InlineKeyboardMarkup()
+    btn1 = types.InlineKeyboardButton(text= "btn1", callback_data="btn1")
+    btn2 = types.InlineKeyboardButton(text= "btn2", callback_data="btn2")
+    markup.add(btn1,btn2)
+    bot.send_message(message.chat.id, "1", reply_markup=markup)
+    
+@bot.callback_query_handler(func=lambda callback: callback.data)
+def check_callbackdata(callback):
+    if callback.data == "btn1":
+         markup = types.InlineKeyboardMarkup()
+         btn1 = types.InlineKeyboardButton(text= "btn2", callback_data="btn2")
+         btn2 = types.InlineKeyboardButton(text= "btn2", callback_data="btn1")
+         markup.add(btn1,btn2)
+         bot.edit_message_text(chat_id=callback.message.chat.id, message_id=callback.message.id, text="ancora", reply_markup=markup) 
+        
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
     bot.reply_to(message, message.text)
